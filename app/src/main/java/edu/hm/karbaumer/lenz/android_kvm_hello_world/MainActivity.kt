99c8fb5873b644a31ff6c0fa3b01d7ceaa21869b
Lenz Karbaumer <lenz.karbaumer@hm.edu>
2021-08-31 12:24:57 +0200

    Initial commit

---
+++ app/src/main/java/edu/hm/karbaumer/lenz/android_kvm_hello_world/MainActivity.kt
@@ -0,0 +1,34 @@
+package edu.hm.karbaumer.lenz.android_kvm_hello_world
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import edu.hm.karbaumer.lenz.android_kvm_hello_world.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'android_kvm_hello_world' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'android_kvm_hello_world' library on application startup.
+        init {
+            System.loadLibrary("android_kvm_hello_world")
+        }
+    }
+}
